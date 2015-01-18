41c010f34f6a27bd06880ff9046363731b8d9189
RadioLemon <horsentp@gmail.com>
2015-01-18 16:50:42 -0600

    win state and game over state

--- TAG_Project/src/tag_project/GameOverState.java
+++ TAG_Project/src/tag_project/GameOverState.java
@@ -7,8 +7,10 @@ package tag_project;
 
 import bropals.lib.simplegame.gui.GuiGroup;
 import bropals.lib.simplegame.gui.GuiImage;
+import bropals.lib.simplegame.gui.GuiText;
 import bropals.lib.simplegame.state.GameState;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
@@ -30,17 +32,27 @@ public class GameOverState extends GameState {
     
     @Override
     public void update() {
+        
     }
 
     @Override
     public void render(Object graphicsObj) {
+        Graphics g = (Graphics)graphicsObj;
+        gui.render(graphicsObj);
     }
 
     @Override
     public void onEnter() {
         gui = new GuiGroup();
-        font = new Font(Font.SANS_SERIF, Font.BOLD, 20);
+        font = new Font(Font.SANS_SERIF, Font.BOLD, 32);
         gui.addElement(new GuiImage(0, 0, 800, 600, getImage("gameOverBackground")));
+        GuiText b, f;
+        b = new GuiText("" + biscuits, 160, 380, 0, 0, false);
+        b.setFont(font);
+        f = new GuiText("" + furniture, 510, 380, 0, 0, false);
+        f.setFont(font);
+        gui.addElement(b);
+        gui.addElement(f);
     }
 
     @Override
@@ -49,7 +61,7 @@ public class GameOverState extends GameState {
 
     @Override
     public void key(KeyEvent kr, boolean pressed) {
-        if (kr.getKeyCode() == KeyEvent.VK_SPACE) {
+        if (kr.getKeyCode() == KeyEvent.VK_ESCAPE && pressed) {
             getGameStateRunner().setState(new MainMenuState());
         }
     }
